d5c105d6230572ed1114c2d98b262c823727d8e4
Wassim Beltaief <beltaief.wassim@gmail.com>
2026-09-13 14:50:57 +0200

    chore: bump version to 1.0.1

--- loupe-runtime/build.gradle.kts
+++ loupe-runtime/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 mavenPublishing {
-    coordinates("com.wassimbeltaief", "loupe-runtime", "1.0.0-alpha01")
+    coordinates("com.wassimbeltaief", "loupe-runtime", "1.0.1")
     pom {
         name.set("Loupe Runtime")
         description.set("Zero-instrumentation recomposition debugger for Jetpack Compose")
